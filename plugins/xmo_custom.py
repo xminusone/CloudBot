f17d614a8927d59bbbe65fc6097a555b534af353
x_minus_one <xminusone@users.noreply.github.com>
2016-01-23 22:31:55 -0500

    Create xmo_custom.py

---
+++ plugins/xmo_custom.py
@@ -0,0 +1,8 @@
+# Custom CloudBot Commands #
+# /u/x_minus_one - 2016    #
+# -------------------------#
+from cloudbot import hook
+
+@hook.command (openthepodbaydoors)
+def podbay(text):
+  return "I'm sorry, I'm afraid I can't do that."
